d86fe5ed3dc95d8714ae687cb6da7259563955a0
Michael Voigt <Michael@MichaelVoigt.de>
2021-07-03 17:27:20 +0200

    Second Commit

--- class LightBarrierController.java
+++ class LightBarrierController.java
@@ -1,4 +1,4 @@
-class main{  
+class LightBarrierController{  
     public static void main(String args[]){  
      System.out.println("I'm the Light Barrier Controller controlling the Light Barrier.");  
     }  
