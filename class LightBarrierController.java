8dd79b24917920b130e84c2b2d44dbaab8840020
Michael Voigt <Michael@MichaelVoigt.de>
2021-07-04 00:14:42 +0200

    Sixth Commit

--- class LightBarrierController.java
+++
@@ -1,5 +0,0 @@
-class LightBarrierController{  
-    public static void main(String args[]){  
-     System.out.println("I'm the Light Barrier Controller controlling the Light Barrier.");  
-    }  
-}  
